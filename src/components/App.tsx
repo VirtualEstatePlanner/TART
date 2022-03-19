e5503ce3afa4643b28d29ae9099792923f5cfcf8
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-03-19 17:21:05 -0400

    removed MD header section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,5 @@
 /** @format */
 
-//  App.tsx
-//  MEGADocker
-//  a React Component that renders the entire MEGADocker application
-//  Created by George Georgulas IV on 3/17/19.
-//  Copyright © 2019-2022 The MegaDocker Group. All rights reserved.
-
 import * as React from 'react'
 
 export const App: React.FC = (): React.ReactElement => {
