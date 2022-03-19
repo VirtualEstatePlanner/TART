3e8f5e6ed7ad0600ae41139800189bfd8ef47482
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2022-03-19 17:18:04 -0400

    removed old components from MD

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,26 +7,10 @@
 //  Copyright © 2019-2022 The MegaDocker Group. All rights reserved.
 
 import * as React from 'react'
-import { Grid, Paper } from '@mui/material'
-import { ContainerManikinPane } from './ContainerManikinPane'
-import { ContainerMainArea } from './ContainerMainArea'
-import { MegaProvider } from './MegaContext'
 
 export const App: React.FC = (): React.ReactElement => {
   return (
     <div className='App'>
-      <MegaProvider>
-        <Paper square>
-          <Grid container>
-            <Grid item xs={2}>
-              <ContainerManikinPane />
-            </Grid>
-            <Grid item xs={10}>
-              <ContainerMainArea />
-            </Grid>
-          </Grid>
-        </Paper>
-      </MegaProvider>
     </div>
   )
 }
